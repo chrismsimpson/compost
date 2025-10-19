686e2c24c92ffdbcf83ff0ad0448183ef4a4e6a9
chrismsimpson <chris@deepscroll.com>
2025-10-19 16:48:15 +1100

    grid dots

--- apps/rex/src/app/three/SceneCanvas.tsx
+++ apps/rex/src/app/three/SceneCanvas.tsx
@@ -1,34 +1,34 @@
 'use client';
 
-import { Canvas, useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
-import * as THREE from 'three';
-
-function SpinningBox() {
-  const ref = useRef<THREE.Mesh>(null!);
-
-  useFrame((_, delta) => {
-    // simple rotation animation
-    ref.current.rotation.x += delta;
-    ref.current.rotation.y += delta * 1.2;
-  });
-
-  return (
-    <mesh ref={ref}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="hotpink" />
-    </mesh>
-  );
-}
+import { Canvas } from '@react-three/fiber';
+import { useMemo } from 'react';
+import { buildGridPoints, TileLayout, type TileLayoutKey } from './grid';
 
 export default function SceneCanvas() {
-  // inline styles to avoid any Tailwind coupling here
+  // change to TileLayout.square to compare layouts
+  // const layout: TileLayoutKey = TileLayout.hex;
+  const layout: TileLayoutKey = TileLayout.square;
+
+  const points = useMemo(() => buildGridPoints(layout, 4, 1), [layout]);
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas camera={{ position: [2.5, 2.5, 2.5], fov: 60 }}>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 5, 5]} intensity={0.8} />
-        <SpinningBox />
+      {/* Move camera to a simple isometric-ish angle */}
+      <Canvas camera={{ position: [8, 8, 8], fov: 60 }}>
+        <ambientLight intensity={0.6} />
+        <directionalLight position={[5, 10, 5]} intensity={0.8} />
+        {/* Helpful axes (X: red, Y: green, Z: blue) */}
+        <axesHelper args={[5]} />
+
+        {/* Rotate the grid from the XY plane down onto the XZ plane */}
+        <group rotation={[-Math.PI / 2, 0, 0]}>
+          {points.map((p, i) => (
+            <mesh key={i} position={p as [number, number, number]}>
+              <sphereGeometry args={[0.06, 12, 12]} />
+              <meshStandardMaterial color="orange" />
+            </mesh>
+          ))}
+        </group>
       </Canvas>
     </div>
   );
